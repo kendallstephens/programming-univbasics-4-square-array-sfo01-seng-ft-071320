0576f56bd6c20ec2cb81eda4b4f60cfebff6d603
Kendall Stephens <kendoll.stephens@gmail.com>
2020-06-30 21:56:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(numbers)
   new_numbers = []
   counter = 0
  while counter < numbers.length do
-   counter += 1
  new_numbers.push(numbers[counter]**2)
+ counter += 1
  end
   return new_numbers
 end
